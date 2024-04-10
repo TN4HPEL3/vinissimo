78b05d77472039f7b203cd05f2529e34e68c0239
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-10 12:16:57 +0200

    Hover on buttons

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -4,6 +4,8 @@ function Button({ text, icon, handleClick }) {
   return (
     <button type="button" onClick={handleClick} className={css.button}>
       {text} {icon}
+      <div className={css.button__horizontal}></div>
+      <div className={css.button__vertical}></div>
     </button>
   );
 }
