544e3cdb24c0e94bbebc8926f740d06bfc3fd0e6
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-11 21:08:04 +0200

    Create section template

---
+++ src/components/templates/Section/Section.jsx
@@ -0,0 +1,10 @@
+function Section({ title, children }) {
+  return (
+    <div className={css.section}>
+      {title && <h2>{title}</h2>}
+      {children}
+    </div>
+  );
+}
+
+export { Section };
