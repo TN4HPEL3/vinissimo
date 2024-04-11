1103f9501bfd10da455ec5a844fbc068c0bc295d
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-11 13:33:57 +0200

    Styles for drop-down menu

--- src/components/organisms/DropDownMenu/DropDownMenu.jsx
+++ src/components/organisms/DropDownMenu/DropDownMenu.jsx
@@ -1,15 +1,17 @@
 import { nanoid } from 'nanoid';
 
+import css from './DropDownMenu.module.css';
+
 function DropDownMenu({ options, handleClick }) {
   const listItems = options.map(item => {
     const id = nanoid();
     return (
-      <li key={id} onclick={handleClick}>
+      <li key={id} onClick={handleClick}>
         {item}
       </li>
     );
   });
-  return <ul>{listItems}</ul>;
+  return <ul className={css.dropdownMenu}>{listItems}</ul>;
 }
 
 export { DropDownMenu };
