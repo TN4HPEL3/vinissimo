1e601e556e624eddaf2255755b7c133a423110f2
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-11 15:46:43 +0200

    Add opening mobile menu, we still need to add styles and svg

--- src/components/templates/MobileMenuButton/MobileMenuButton.jsx
+++ src/components/templates/MobileMenuButton/MobileMenuButton.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+
 import { Icon } from '../../atoms/Icon/Icon.jsx';
+import { HeaderNavigation } from '../HeaderNavigation/HeaderNavigation.jsx';
 
 import css from './MobileMenuButton.module.css';
 
 function MobileMenuButton() {
+  const [isOpen, setIsOpen] = useState(false);
   const handleClick = () => {
-    console.log('Button clicked!');
+    setIsOpen(prevState => !prevState);
   };
   return (
-    <button className={css.mobileMenuButton} onClick={handleClick}>
-      <Icon id="burger-menu" />
-    </button>
+    <>
+      <button className={css.mobileMenuButton} onClick={handleClick}>
+        <Icon id="burger-menu" />
+      </button>
+      {isOpen && (
+        <div>
+          <button className={css.closeButton} onClick={handleClick}>
+            X
+          </button>
+          <HeaderNavigation className={css.header__navigation} />
+        </div>
+      )}
+    </>
   );
 }
 
