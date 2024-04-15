c2847b2e32317df67db5776bda93f8c177e3bf68
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-15 16:59:41 +0200

    Remove AboutTemplate from SharedLAyout

--- src/components/templates/SharedLayout/SharedLayout.jsx
+++ src/components/templates/SharedLayout/SharedLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
-import { About } from '../About/About.jsx';
 // import { Header } from '../Header/Header.jsx';
 // import { Footer } from '../Footer/Footer.jsx';
 // import { Container } from '../Container/Container.jsx';
@@ -10,7 +9,6 @@ function SharedLayout() {
     <>
       {/*  <Container>
      <Header /> */}
-      <About />
       <Outlet />
       {/* <Footer />
     </Container> */}
