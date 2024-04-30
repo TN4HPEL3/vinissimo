c611a1cbd46defa872f05cfc551d2e7491c876a6
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-30 19:26:50 +0200

    Components to reservation template

--- src/components/templates/Reservatiom/ReservationTemplate.jsx
+++ src/components/templates/Reservatiom/ReservationTemplate.jsx
@@ -1,4 +1,8 @@
 import { CheckMediaQuery } from '../../../helpers/checkMediaQuery';
+import { ContactLinkAtom } from '../../../pages/TestPage/atoms/ContactLinkAtom/ContactLinkAtom';
+import { ParagraphAtom } from '../../atoms/ParagraphAtom';
+import { ReservationMapAtom } from '../../atoms/Reservation/ReservationMapAtom';
+import { ReservationTextAtom } from '../../atoms/Reservation/ReservationTextAtom';
 import { SectionTemplate } from '../Section/SectionTemplate';
 
 import css from './ReservationTemplate.module.css';
@@ -14,9 +18,18 @@ function ReservationTemplate() {
           </>
         ) : (
           <>
-            <p>Desktop</p>
+            <ReservationMapAtom />
           </>
         )}
+        <div>
+          <ParagraphAtom text={<ReservationTextAtom />} />
+          <button type="button">
+            <ContactLinkAtom
+              href="https://mojstolik.pl/restauracja/sztuczka-bistro-wino/c04bbd833ddf1bc466c75b19370dceaa/"
+              text="rezerwacja"
+            />
+          </button>
+        </div>
       </div>
     </SectionTemplate>
   );
