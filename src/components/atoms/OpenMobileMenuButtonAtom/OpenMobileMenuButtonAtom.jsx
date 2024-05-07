bea0d9899886a03953eec2f6b1b2cfdaa4072efd
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-07 19:32:53 +0200

    Open mobile menu class and styles

--- src/components/atoms/OpenMobileMenuButtonAtom/OpenMobileMenuButtonAtom.jsx
+++ src/components/atoms/OpenMobileMenuButtonAtom/OpenMobileMenuButtonAtom.jsx
@@ -1,6 +1,11 @@
+import css from './OpenMobileMenuButtonAtom.module.css';
+
 function OpenMobileMenuButtonAtom({ handleClick }) {
   return (
-    <button onClick={handleClick}>
+    <button
+      className={[css['header__mobile-menu-button']]}
+      onClick={handleClick}
+    >
       {/* <IconAtom id="burger-menu" /> */}
       Open
     </button>
