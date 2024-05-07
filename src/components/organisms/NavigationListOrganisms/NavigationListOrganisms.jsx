fb0dc34141ee01616432cb9b2e4ccb39070a63dc
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-07 18:30:08 +0200

    Function for opening mobile menu

--- src/components/organisms/NavigationListOrganisms/NavigationListOrganisms.jsx
+++ src/components/organisms/NavigationListOrganisms/NavigationListOrganisms.jsx
@@ -1,8 +1,16 @@
-import { NavigationListItemMolecule } from '../../molecules/NavigationListItemMolecule/NavigationListItemMolecule';
+import { useState } from 'react';
+
+import { MobileMenuOrganism } from '../MobileMenuOrganism/MobileMenuOrganism.jsx';
+import { OpenMobileMenuButtonAtom } from '../../atoms/OpenMobileMenuButtonAtom/OpenMobileMenuButtonAtom.jsx';
+import { NavigationListItemMolecule } from '../../molecules/NavigationListItemMolecule/NavigationListItemMolecule.jsx';
 
 import css from './NavigationListOrganisms.module.css';
 
 function NavigationListOrganisms() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className={css.header__content}>
       <nav className={css.header__navigation}>
@@ -13,6 +21,8 @@ function NavigationListOrganisms() {
         <NavigationListItemMolecule item={'team'} />
         <NavigationListItemMolecule item={'rezerwacja'} />
       </nav>
+      <OpenMobileMenuButtonAtom handleClick={handleClick} />
+      {isOpen && <MobileMenuOrganism handleClick={handleClick} />}
     </div>
   );
 }
