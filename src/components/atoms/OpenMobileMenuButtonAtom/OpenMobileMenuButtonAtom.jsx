4978339c770823daa60cdc8a52656bbd06e531b1
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-07 18:26:25 +0200

    Open menu button

---
+++ src/components/atoms/OpenMobileMenuButtonAtom/OpenMobileMenuButtonAtom.jsx
@@ -0,0 +1,10 @@
+function OpenMobileMenuButtonAtom({ handleClick }) {
+  return (
+    <button onClick={handleClick}>
+      {/* <IconAtom id="burger-menu" /> */}
+      Open
+    </button>
+  );
+}
+
+export { OpenMobileMenuButtonAtom };
