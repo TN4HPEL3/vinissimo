da71e7a8a2e02e907b3cd87759958a1ff3cf19df
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-11 11:08:41 +0200

    Fix structure of template using href from helpers, move reservation text to separate molecule

--- src/components/templates/Reservation/ReservationTemplate.jsx
+++ src/components/templates/Reservation/ReservationTemplate.jsx
@@ -1,15 +1,11 @@
-import { ParagraphAtom } from '../../atoms/ParagraphAtom';
+import { hrefReservation } from '../../../helpers/findLink';
 import { SectionTemplate } from '../Section/SectionTemplate';
 import { ButtonAtom } from '../../atoms/buttonAtom/ButtonAtom';
-import { ContactLinkAtom } from '../../../pages/TestPage/atoms/ContactLinkAtom/ContactLinkAtom';
-import { ReservationTextAtom } from '../../atoms/Reservation/ReservationTextAtom/ReservationTextAtom';
+import { ContactLinkAtom } from '../../atoms/ContactLinkAtom/ContactLinkAtom';
 import { ReservationMapMolecule } from '../../molecules/ReservationMapMolecule/ReservationMapMolecule';
+import { ReservationTextMolecule } from '../../molecules/ReservationTextMolecule/ReservationTextMolecule';
 
 import css from './ReservationTemplate.module.css';
-
-// To fix:
-// 1. Href take from JSON
-// 2. ContactLinkAtom  - styles from Test page to delete
 
 function ReservationTemplate() {
   return (
@@ -19,15 +15,12 @@ function ReservationTemplate() {
         <div className={css.reservation}>
           <ReservationMapMolecule />
           <div className={css.reservation__content}>
-            <ParagraphAtom
-              className={css.reservation__content__text}
-              text={<ReservationTextAtom />}
-            />
+            <ReservationTextMolecule />
             <ButtonAtom
               text={
                 <ContactLinkAtom
-                  href="https://mojstolik.pl/restauracja/sztuczka-bistro-wino/c04bbd833ddf1bc466c75b19370dceaa/"
-                  text="zarezerwuj stolik"
+                  href={hrefReservation}
+                  children="zarezerwuj stolik"
                 />
               }
             />
