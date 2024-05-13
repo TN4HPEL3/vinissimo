0a2ec4ee1eb7b627b6cc54a7f1124e0387c5c7dc
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-13 12:12:40 +0200

    Address molecule

---
+++ src/components/molecules/FooterAddressMolecule/FooterAddressMolecule.jsx
@@ -0,0 +1,12 @@
+import { ParagraphAtom } from '../../atoms/ParagraphAtom';
+
+function FooterAddressMolecule() {
+  return (
+    <div>
+      <ParagraphAtom text="Vinissimo Restaurant & Wine Bar" />
+      <ParagraphAtom text="ul. Bema 6 Sopot" />
+    </div>
+  );
+}
+
+export { FooterAddressMolecule };
