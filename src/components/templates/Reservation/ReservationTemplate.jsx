79cf2c98e4ef7cb9e3f5b80c2dad45bb63a0cad5
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-13 17:18:41 +0200

    Create Reservation Organism and change structure of the template

--- src/components/templates/Reservation/ReservationTemplate.jsx
+++ src/components/templates/Reservation/ReservationTemplate.jsx
@@ -1,33 +1,10 @@
-import { hrefReservation } from '../../../helpers/findLink';
 import { SectionTemplate } from '../Section/SectionTemplate';
-import { ButtonAtom } from '../../atoms/buttonAtom/ButtonAtom';
-import { ContactLinkAtom } from '../../atoms/ContactLinkAtom/ContactLinkAtom';
-import { ReservationMapMolecule } from '../../molecules/ReservationMapMolecule/ReservationMapMolecule';
-import { ReservationTextMolecule } from '../../molecules/ReservationTextMolecule/ReservationTextMolecule';
 
-import css from './ReservationTemplate.module.css';
+import { ReservationOrganism } from '../../organisms/ReservationOrganism/ReservationOrganism';
 
 function ReservationTemplate() {
   return (
-    <SectionTemplate
-      title="rezerwacja"
-      children={
-        <div className={css.reservation}>
-          <ReservationMapMolecule />
-          <div className={css.reservation__content}>
-            <ReservationTextMolecule />
-            <ButtonAtom
-              text={
-                <ContactLinkAtom
-                  href={hrefReservation}
-                  children="zarezerwuj stolik"
-                />
-              }
-            />
-          </div>
-        </div>
-      }
-    ></SectionTemplate>
+    <SectionTemplate title="rezerwacja" children={<ReservationOrganism />} />
   );
 }
 
