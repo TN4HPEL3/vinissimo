316065d35cad5185c0030fbfb486f9b1fa276461
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-15 11:02:32 +0200

    Add navigation template to shared layout

--- src/components/templates/SharedLayout/SharedLayout.jsx
+++ src/components/templates/SharedLayout/SharedLayout.jsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 // import { Header } from '../Header/Header.jsx';
 // import { Footer } from '../Footer/Footer.jsx';
 import { Container } from '../Container/Container.jsx';
+import { NavigationTemplate } from '../NavigationTemplate/NavigationTemplate.jsx';
 
 function SharedLayout() {
   return (
     <>
       <Container>
         {/* <Header /> */}
+        <NavigationTemplate />
         <Outlet />
         {/* <Footer /> */}
       </Container>
