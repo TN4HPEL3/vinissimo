638666b4773afd243ce5eafe385c8f91b034d193
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-15 13:13:01 +0200

    Change structure of organism

--- src/components/organisms/MobileMenuOrganism/MobileMenuOrganism.jsx
+++ src/components/organisms/MobileMenuOrganism/MobileMenuOrganism.jsx
@@ -1,24 +1,16 @@
 import { CloseMobileMenuButtonAtom } from '../../atoms/CloseMobileMenuButtonAtom/CloseMobileMenuButtonAtom';
 import { NavigationListItemMolecule } from '../../molecules/NavigationListItemMolecule/NavigationListItemMolecule';
 
-import css from './MobileMenuOrganism.module.css';
-
-function MobileMenuOrganism({ handleClick, isOpen }) {
+function MobileMenuOrganism({ handleClick }) {
   return (
     <>
-      <nav
-        className={`${css['mobile-menu']} ${
-          isOpen ? css['mobile-menu--open'] : ''
-        }`}
-      >
-        <CloseMobileMenuButtonAtom handleClick={handleClick} />
-        <NavigationListItemMolecule item={'home'} />
-        <NavigationListItemMolecule item={'menu'} />
-        <NavigationListItemMolecule item={'wino'} />
-        <NavigationListItemMolecule item={'eventy'} />
-        <NavigationListItemMolecule item={'team'} />
-        <NavigationListItemMolecule item={'rezerwacja'} />
-      </nav>
+      <CloseMobileMenuButtonAtom handleClick={handleClick} />
+      <NavigationListItemMolecule item={'home'} />
+      <NavigationListItemMolecule item={'menu'} />
+      <NavigationListItemMolecule item={'wino'} />
+      <NavigationListItemMolecule item={'eventy'} />
+      <NavigationListItemMolecule item={'team'} />
+      <NavigationListItemMolecule item={'rezerwacja'} />
     </>
   );
 }
