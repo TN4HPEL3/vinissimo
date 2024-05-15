932bf44ac87a13317d65c6fe688c5d009fe3c517
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-15 10:50:46 +0200

    Alcohol basic template

---
+++ src/pages/Menu/templates/MenuPageAlcoholTemplate/MenuPageAlcoholTemplate.jsx
@@ -0,0 +1,11 @@
+import { MenuPageOrganism } from '../../organisms/MenuPageOrganism/MenuPageOrganism';
+
+function MenuPageAlcoholTemplate() {
+  return (
+    <MenuPageOrganism>
+      <p>ALCOHOLE STRONA</p>
+    </MenuPageOrganism>
+  );
+}
+
+export { MenuPageAlcoholTemplate };
