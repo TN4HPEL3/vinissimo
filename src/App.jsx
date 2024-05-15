4b1602e29f671d6b4e7737f794c4cc4eb17f334f
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-15 13:30:01 +0200

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { SharedLayout } from './components/templates/SharedLayout/SharedLayout';
+import { HomePage } from './pages/Home/HomePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        {/* <Route index element={<Home />} /> */}
+        <Route index element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
       </Route>
+
       {/* <Route path="*" element={<Page404 />} /> */}
     </Routes>
   );
