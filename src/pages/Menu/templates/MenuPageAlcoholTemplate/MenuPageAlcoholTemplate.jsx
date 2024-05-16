88125aeaea1b57f7f6a6690d192ef06309873cb9
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-16 12:06:59 +0200

    Menu alcohol template

--- src/pages/Menu/templates/MenuPageAlcoholTemplate/MenuPageAlcoholTemplate.jsx
+++ src/pages/Menu/templates/MenuPageAlcoholTemplate/MenuPageAlcoholTemplate.jsx
@@ -1,10 +1,46 @@
-import { MenuPageOrganism } from '../../organisms/MenuPageOrganism/MenuPageOrganism';
+import { MenuPageTemplate } from '../MenuPageTemplate/MenuPageTemplate';
+import { MenuAlcoholsOrganism } from '../../organisms/MenuAlcoholsOrganism/MenuAlcoholsOrganism';
+
+import menu from '../../../../data/alcohols.json';
+
+import css from './MenuPageAlcoholTemplate.module.css';
 
 function MenuPageAlcoholTemplate() {
   return (
-    <MenuPageOrganism>
-      <p>ALCOHOLE STRONA</p>
-    </MenuPageOrganism>
+    <MenuPageTemplate className={css.menu__alcohols}>
+      <div className={css.menu__alcohols__content}>
+        <div>
+          <MenuAlcoholsOrganism
+            category={'cocktails'}
+            data={menu}
+            title={'cocktails'}
+          />
+          <MenuAlcoholsOrganism
+            category={'apéritif'}
+            data={menu}
+            title={'apéritif'}
+          />
+          <MenuAlcoholsOrganism
+            category={'vodka'}
+            data={menu}
+            title={'vodka | tincture'}
+          />
+        </div>
+        <div>
+          <MenuAlcoholsOrganism
+            category={'highlandPark'}
+            data={menu}
+            title={'highland park single malt whisky'}
+          />
+          <MenuAlcoholsOrganism
+            category={'whisky'}
+            data={menu}
+            title={'whisky | bourbon | whiskey'}
+          />
+          <MenuAlcoholsOrganism category={'rum'} data={menu} title={'rum'} />
+        </div>
+      </div>
+    </MenuPageTemplate>
   );
 }
 
