23bc258abe8af19032ffe1b36a1bf463d79c67e6
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-17 14:43:55 +0200

    Alcohol template with styles

--- src/pages/Menu/templates/MenuPageAlcoholTemplate/MenuPageAlcoholTemplate.jsx
+++ src/pages/Menu/templates/MenuPageAlcoholTemplate/MenuPageAlcoholTemplate.jsx
@@ -1,5 +1,4 @@
-import FlipPage from 'react-flip-page';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { MenuAlcoholsOrganism } from '../../organisms/MenuAlcoholsOrganism/MenuAlcoholsOrganism';
 import { MenuPageButtonsOrganism } from '../../organisms/MenuPageButtonsOrganism/MenuPageButtonsOrganism';
@@ -9,129 +8,104 @@ import menu from '../../../../data/alcohols.json';
 import css from './MenuPageAlcoholTemplate.module.css';
 
 function MenuPageAlcoholTemplate() {
-  const flipPageRef = useRef(null);
   const [pageNumber, setPageNumber] = useState(0);
-  const maxNumberOfPages = 1;
-
+  const numberOfPages = 1;
   const goToPreviousPage = () => {
-    if (flipPageRef.current) {
-      flipPageRef.current.gotoPreviousPage();
-      setPageNumber(prevPage => prevPage - 1);
-    }
+    setPageNumber(pageNumber - 1);
   };
   const goToNextPage = () => {
-    if (flipPageRef.current) {
-      flipPageRef.current.gotoNextPage();
-      setPageNumber(prevPage => prevPage + 1);
-    }
+    setPageNumber(pageNumber + 1);
   };
 
   return (
-    <div className={css.menu__alcohols}>
+    <div className={css.menu}>
       <MenuPageButtonsOrganism />
-      {pageNumber > 0 && (
-        <button onClick={goToPreviousPage} type="button">
-          prev
-        </button>
-      )}
-      {maxNumberOfPages !== pageNumber && (
-        <button onClick={goToNextPage} type="button">
-          next
-        </button>
-      )}
-      <p className={css.menu__alcohols__page}>
-        Strona {pageNumber + 1} z {maxNumberOfPages}
+      <p>
+        Strona {pageNumber + 1} z {numberOfPages + 1}
       </p>
-      <div className={css.menu__alcohols__content}>
-        <FlipPage
-          ref={flipPageRef}
-          orientation="horizontal"
-          uncutPages={true}
-          responsive={true}
-          flipOnTouch={true}
-          showSwipeHint={true}
-          onPageChange={e => setPageNumber(e)}
-        >
-          <article>
-            <div className={css['menu__alcohols__content--page']}>
-              <div>
-                <MenuAlcoholsOrganism
-                  category={'cocktails'}
-                  data={menu}
-                  title={'cocktails'}
-                />
-                <MenuAlcoholsOrganism
-                  category={'apéritif'}
-                  data={menu}
-                  title={'apéritif'}
-                />
-                <MenuAlcoholsOrganism
-                  category={'vodka'}
-                  data={menu}
-                  title={'vodka | tincture'}
-                />
-              </div>
-              <div>
-                <MenuAlcoholsOrganism
-                  category={'highlandPark'}
-                  data={menu}
-                  title={'highland park single malt whisky'}
-                />
-                <MenuAlcoholsOrganism
-                  category={'whisky'}
-                  data={menu}
-                  title={'whisky | bourbon | whiskey'}
-                />
-                <MenuAlcoholsOrganism
-                  category={'rum'}
-                  data={menu}
-                  title={'rum'}
-                />
-              </div>
-            </div>
-          </article>
-          <article>
-            <div className={css['menu__alcohols__content--page']}>
-              <div>
-                <MenuAlcoholsOrganism
-                  category={'tequila'}
-                  data={menu}
-                  title={'tequila'}
-                />
-                <MenuAlcoholsOrganism
-                  category={'gin'}
-                  data={menu}
-                  title={'gin'}
-                />
-                <MenuAlcoholsOrganism
-                  category={'cognac'}
-                  data={menu}
-                  title={'cognac | brandy | armagnac'}
-                />
-              </div>
-              <div>
-                <MenuAlcoholsOrganism
-                  category={'vermouth'}
-                  data={menu}
-                  title={'vermouth'}
-                />
+      <div className={css['menu__page__buttons']}>
+        {pageNumber > 0 && (
+          <button onClick={goToPreviousPage} type="button">
+            prev
+          </button>
+        )}
 
-                <MenuAlcoholsOrganism
-                  category={'digestif'}
-                  data={menu}
-                  title={'digestif'}
-                />
-
-                <MenuAlcoholsOrganism
-                  category={'liqueurs'}
-                  data={menu}
-                  title={'liqueurs'}
-                />
-              </div>
-            </div>
-          </article>
-        </FlipPage>
+        {numberOfPages !== pageNumber && (
+          <button onClick={goToNextPage} type="button">
+            next
+          </button>
+        )}
       </div>
+      {pageNumber === 0 && (
+        <div className={css['menu__alcohols__content--page']}>
+          <div>
+            <MenuAlcoholsOrganism
+              category={'cocktails'}
+              data={menu}
+              title={'cocktails'}
+            />
+            <MenuAlcoholsOrganism
+              category={'apéritif'}
+              data={menu}
+              title={'apéritif'}
+            />
+            <MenuAlcoholsOrganism
+              category={'vodka'}
+              data={menu}
+              title={'vodka | tincture'}
+            />
+          </div>
+          <div>
+            <MenuAlcoholsOrganism
+              category={'highlandPark'}
+              data={menu}
+              title={'highland park single malt whisky'}
+            />
+            <MenuAlcoholsOrganism
+              category={'whisky'}
+              data={menu}
+              title={'whisky | bourbon | whiskey'}
+            />
+            <MenuAlcoholsOrganism category={'rum'} data={menu} title={'rum'} />
+          </div>
+        </div>
+      )}
+      {pageNumber === 1 && (
+        <div className={css['menu__alcohols__content--page']}>
+          <div>
+            <MenuAlcoholsOrganism
+              category={'tequila'}
+              data={menu}
+              title={'tequila'}
+            />
+            <MenuAlcoholsOrganism category={'gin'} data={menu} title={'gin'} />
+            <MenuAlcoholsOrganism
+              category={'cognac'}
+              data={menu}
+              title={'cognac | brandy | armagnac'}
+            />
+          </div>
+          <div>
+            <MenuAlcoholsOrganism
+              category={'vermouth'}
+              data={menu}
+              title={'vermouth'}
+            />
+
+            <MenuAlcoholsOrganism
+              category={'digestif'}
+              data={menu}
+              title={'digestif'}
+            />
+
+            <MenuAlcoholsOrganism
+              category={'liqueurs'}
+              data={menu}
+              title={'liqueurs'}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
