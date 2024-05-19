356b244ea210bedd4746700232414c5122efc1df
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-19 10:49:22 +0200

    Fix adding className for active link using helper

--- src/components/atoms/NavigationListItemAtom/NavigationListItemAtom.jsx
+++ src/components/atoms/NavigationListItemAtom/NavigationListItemAtom.jsx
@@ -1,17 +1,12 @@
 import { NavLink } from 'react-router-dom';
 
+import { isLinkActive } from '../../../helpers/isLinkActive';
+
 import css from './NavigationListItemAtom.module.css';
 
 function NavigationListItemAtom({ element, text }) {
   return (
-    <NavLink
-      className={({ isActive }) =>
-        isActive
-          ? [css['header__navigation__link--active']]
-          : [css['header__navigation__link']]
-      }
-      to={element}
-    >
+    <NavLink className={isActive => css[isLinkActive(isActive)]} to={element}>
       {text}
     </NavLink>
   );
