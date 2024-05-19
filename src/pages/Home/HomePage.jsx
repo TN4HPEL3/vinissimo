2d3d24eca0953ea196dabc6c09c1014d57e0bd7d
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-20 00:37:20 +0200

    Add hero template

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,13 @@
 import { MenuTemplate } from '../../components/templates/Menu/MenuTemplate';
 import { AboutTemplate } from '../../components/templates/About/AboutTemplate';
 import { RateUsTemplate } from '../../components/templates/RateUs/RateUsTemplate';
+import { HeroTemplate } from '../../components/templates/HeroTemplate/HeroTemplate';
 import { ReservationTemplate } from '../../components/templates/Reservation/ReservationTemplate';
 
 function HomePage() {
   return (
     <>
-      {/* <HeroTemplate/> */}
+      <HeroTemplate />
       <AboutTemplate />
       <MenuTemplate />
       <RateUsTemplate />
