530f31648b19a2df49300ff1ade0ce2b49272711
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-19 13:47:50 +0200

    Routes for menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { SharedLayout } from './components/templates/SharedLayout/SharedLayout';
+import { WinePage } from './pages/Wine/WinePage.jsx';
+import { SharedLayout } from './components/templates/SharedLayout/SharedLayout.jsx';
+import { WineGlassesTemplate } from './pages/Wine/templates/WineGlassesTemplate.jsx';
+import { MenuPageFoodTemplate } from './pages/Menu/templates/MenuPageFoodTemplate/MenuPageFoodTemplate.jsx';
+import { MenuPageAlcoholTemplate } from './pages/Menu/templates/MenuPageAlcoholTemplate/MenuPageAlcoholTemplate.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         {/* <Route index element={<Home />} /> */}
+        <Route
+          path="/wine/glasses"
+          element={<WinePage children={<WineGlassesTemplate />} />}
+        />
+        <Route path="/wine/bottles" element={<p>WINE bottles</p>} />
+        <Route path="/menu/food" element={<MenuPageFoodTemplate />} />
+        <Route path="/menu/alcohol" element={<MenuPageAlcoholTemplate />} />
       </Route>
       {/* <Route path="*" element={<Page404 />} /> */}
     </Routes>
