ae2be7bc041baeb2d16d55f4f21a53c6d022f641
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-21 19:08:39 +0200

    Change structure of components using context instead of parameters and hook

--- src/components/atoms/OpenMobileMenuButtonAtom/OpenMobileMenuButtonAtom.jsx
+++ src/components/atoms/OpenMobileMenuButtonAtom/OpenMobileMenuButtonAtom.jsx
@@ -1,13 +1,14 @@
+import { useContext } from 'react';
+
 import { IconAtom } from '../IconAtom/IconAtom';
+import { mobileMenuContext } from '../../../helpers/mobileMenuProvider';
 
 import css from './OpenMobileMenuButtonAtom.module.css';
 
-function OpenMobileMenuButtonAtom({ handleClick }) {
+function OpenMobileMenuButtonAtom() {
+  const { open } = useContext(mobileMenuContext);
   return (
-    <button
-      className={[css['header__mobile-menu-button']]}
-      onClick={handleClick}
-    >
+    <button className={[css['header__mobile-menu-button']]} onClick={open}>
       <IconAtom id="burger-menu" />
     </button>
   );
