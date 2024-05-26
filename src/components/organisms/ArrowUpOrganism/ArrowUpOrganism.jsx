d16cfd96072e08b034141934d679420ba7c5fa5a
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-26 10:05:09 +0200

    Change structure of the organism

--- src/components/organisms/ArrowUpOrganism/ArrowUpOrganism.jsx
+++ src/components/organisms/ArrowUpOrganism/ArrowUpOrganism.jsx
@@ -3,15 +3,10 @@ import { ArrowUpMolecule } from '../../molecules/ArrowUpMolecule/ArrowUpMolecule
 
 import css from './ArrowUpOrganism.module.css';
 
-function ArrowUpOrganism({ isVisible }) {
+function ArrowUpOrganism() {
   const handleClick = () => smoothScrollToTop();
   return (
-    <div
-      className={
-        !isVisible ? css['button__arrow--up--hidden'] : css['button__arrow--up']
-      }
-      onClick={handleClick}
-    >
+    <div className={css['button__arrow--up']} onClick={handleClick}>
       <ArrowUpMolecule />
     </div>
   );
