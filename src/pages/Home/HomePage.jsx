2f178c19e16f7f9c364bd2ea7960d8956e4b0e3c
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-05-28 12:42:57 +0200

    Disappearing arrow up when mobile menu is open

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
+
+import { mobileMenuContext } from '../../helpers/mobileMenuProvider';
 import { MenuTemplate } from '../../components/templates/Menu/MenuTemplate';
 import { AboutTemplate } from '../../components/templates/About/AboutTemplate';
 import { RateUsTemplate } from '../../components/templates/RateUs/RateUsTemplate';
 import { HeroTemplate } from '../../components/templates/HeroTemplate/HeroTemplate';
+import { ArrowUpTemplate } from '../../components/templates/ArrowUpTemplate/ArrowUpTemplate';
 import { ReservationTemplate } from '../../components/templates/Reservation/ReservationTemplate';
 
 function HomePage() {
+  const { close } = useContext(mobileMenuContext);
   return (
     <>
       <HeroTemplate />
@@ -12,6 +17,7 @@ function HomePage() {
       <MenuTemplate />
       <RateUsTemplate />
       <ReservationTemplate />
+      {close && <ArrowUpTemplate />}
     </>
   );
 }
