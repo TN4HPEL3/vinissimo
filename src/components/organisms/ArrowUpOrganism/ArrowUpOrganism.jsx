7f63be45df51632bce7805201a4a6b3d8ee53b07
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-06-04 22:06:25 +0200

    Fix function

--- src/components/organisms/ArrowUpOrganism/ArrowUpOrganism.jsx
+++ src/components/organisms/ArrowUpOrganism/ArrowUpOrganism.jsx
@@ -6,9 +6,12 @@ import { ArrowUpMolecule } from '../../molecules/ArrowUpMolecule/ArrowUpMolecule
 import css from './ArrowUpOrganism.module.css';
 
 const ArrowUpOrganism = forwardRef((props, ref) => {
-  const handleClick = () => smoothScrollToTop();
   return (
-    <div ref={ref} className={css['button__arrow--up']} onClick={handleClick}>
+    <div
+      ref={ref}
+      className={css['button__arrow--up']}
+      onClick={() => smoothScrollToTop()}
+    >
       <ArrowUpMolecule />
     </div>
   );
