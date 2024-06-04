a981176237d4ca30940285ab8f6860a970df658b
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-06-04 21:44:46 +0200

    Add container template

--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -1,14 +1,15 @@
-import { Page404Template } from './template/Page404Template';
+import { Page404Template } from './template/Page404Template.jsx';
+import { Container } from '../../components/templates/Container/Container.jsx';
 
 import css from './Page404.module.css';
 
 function Page404() {
   return (
-    // <Container>
-    <div className={css.page404}>
-      <Page404Template />
-    </div>
-    // </Container>
+    <Container>
+      <div className={css.page404}>
+        <Page404Template />
+      </div>
+    </Container>
   );
 }
 
