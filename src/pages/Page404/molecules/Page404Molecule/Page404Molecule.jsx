b214534c7faeb50c9e40945b8df1f9eafe248f64
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-06-05 10:50:47 +0200

    Use button molecule

---
+++ src/pages/Page404/molecules/Page404Molecule/Page404Molecule.jsx
@@ -0,0 +1,23 @@
+import { Page404ErrorAtom } from '../../atoms/Page404ErrorAtom/Page404ErrorAtom.jsx';
+import { Page404ImageAtom } from '../../atoms/Page404ImageAtom/Page404ImageAtom.jsx';
+import { Page404ContentAtom } from '../../atoms/Page404ContentAtom/Page404ContentAtom.jsx';
+import { Page404ButtonMolecule } from '../Page404ButtonMolecule/Page404ButtonMolecule.jsx';
+
+import css from './Page404Molecule.module.css';
+
+function Page404Molecule() {
+  return (
+    <div className={css.page404Molecule}>
+      <div className={css.page404Molecule__content}>
+        <Page404ErrorAtom />
+        <Page404ContentAtom />
+        <div className={css.page404Molecule__image}>
+          <Page404ImageAtom />
+        </div>
+      </div>
+      <Page404ButtonMolecule />
+    </div>
+  );
+}
+
+export { Page404Molecule };
