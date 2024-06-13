3882eecb7e199e310c5479692157e505e1c75066
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-06-13 12:55:57 +0200

    Events organism

--- src/pages/Events/organisms/EventsOrganism.jsx
+++ src/pages/Events/organisms/EventsOrganism.jsx
@@ -1,16 +1,14 @@
+import { EventsImagesMolecule } from '../molecules/EventsImagesMolecule/EventsImagesMolecule.jsx';
 import { EventsContactMolecule } from '../molecules/EventsContactMolecule/EventsContactMolecule.jsx';
 import { EventsDescriptionMolecule } from '../molecules/EventsDescriptionMolecule/EventsDescriptionMolecule.jsx';
-import { EventsImagesMolecule } from '../molecules/EventsImagesMolecule/EventsImagesMolecule.jsx';
-
-import css from './EventsOrganism.module.css';
 
 function EventsOrganism() {
   return (
-    <div className={css.events}>
+    <>
       <EventsContactMolecule />
       <EventsImagesMolecule />
       <EventsDescriptionMolecule />
-    </div>
+    </>
   );
 }
 
