a3353d216c058d7237274587718adb81fa60d6ae
TN4HPEL3 <ctnahpele@icloud.com>
2024-06-17 15:26:47 +0200

    Add smooth scroll to top fn when clicking on menu page link

--- src/pages/Menu/atoms/MenuPageLinkAtom.jsx
+++ src/pages/Menu/atoms/MenuPageLinkAtom.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from 'react-router-dom';
 
 import { isLinkActive } from '../../../helpers/isLinkActive.js';
+import { smoothScrollToTop } from '../../../helpers/smoothScroll.js';
 
 import css from './MenuPageLinkAtom.module.css';
 
 function MenuPageLinkAtom({ element, content }) {
   return (
-    <NavLink className={isActive => css[isLinkActive(isActive)]} to={element}>
+    <NavLink
+      className={isActive => css[isLinkActive(isActive)]}
+      to={element}
+      onClick={smoothScrollToTop}
+    >
       {content}
     </NavLink>
   );
