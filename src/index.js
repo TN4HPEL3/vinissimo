c5ae25d576cea495c686fe2eb7d79c3e1246c703
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-06-30 11:27:21 +0200

    Create App provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App.jsx';
-import { MobileMenuProvider } from './helpers/mobileMenuProvider.jsx';
+import { AppProvider } from './helpers/contexts/appProvider.js';
 
 import './index.css';
 import './style/variables.css';
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <MobileMenuProvider>
+      <AppProvider>
         <App />
-      </MobileMenuProvider>
+      </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
