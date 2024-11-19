0e3d3775942a0cc84a0ba58ad51eacc5fc9bf34e
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-11-19 21:56:36 +0100

    Add holiday opening hours

--- src/components/molecules/FooterHoursMolecule/FooterHoursMolecule.jsx
+++ src/components/molecules/FooterHoursMolecule/FooterHoursMolecule.jsx
@@ -1,10 +1,16 @@
 import { ParagraphAtom } from '../../atoms/ParagraphAtom/ParagraphAtom.jsx';
 
-function FooterHoursMolecule({ textOpeningHours, textOpeningHoursSunday }) {
+function FooterHoursMolecule({
+  textOpeningHours,
+  textOpeningHoursSunday,
+  children,
+}) {
   return (
     <div>
       <ParagraphAtom text={textOpeningHours} />
       <ParagraphAtom text={textOpeningHoursSunday} />
+      <br />
+      {children}
     </div>
   );
 }
