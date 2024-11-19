e88ab5946c7b1df385d552e945bb28b9ffbb6901
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-11-19 21:39:32 +0100

    Add arrow up

--- src/pages/NewYear/NewYearPage.jsx
+++ src/pages/NewYear/NewYearPage.jsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { NewYearTemplate } from './templates/NewYearTemplate.jsx';
+import { mobileMenuContext } from '../../helpers/mobileMenuProvider.jsx';
+import { ArrowUpTemplate } from '../../components/templates/ArrowUpTemplate/ArrowUpTemplate.jsx';
 
 import css from './NewYearPage.module.css';
 
 function NewYearPage() {
+  const { isOpen } = useContext(mobileMenuContext);
   return (
     <div className={css.newYear}>
       <NewYearTemplate />
+      {!isOpen && <ArrowUpTemplate />}
       <Outlet />
     </div>
   );
