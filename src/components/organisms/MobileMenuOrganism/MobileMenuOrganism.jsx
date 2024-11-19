eb84b144755f8109ee8b1b4b82d950b615247a7e
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-11-19 21:42:54 +0100

    Add NY button to mobile menu

--- src/components/organisms/MobileMenuOrganism/MobileMenuOrganism.jsx
+++ src/components/organisms/MobileMenuOrganism/MobileMenuOrganism.jsx
@@ -14,6 +14,11 @@ function MobileMenuOrganism() {
         activeRoutes={['']}
       />
       <NavigationListItemMolecule
+        item="new-year/basic"
+        text={'new year'}
+        activeRoutes={['']}
+      />
+      <NavigationListItemMolecule
         item="menu/food"
         text={t.navigationMenu}
         activeRoutes={['']}
