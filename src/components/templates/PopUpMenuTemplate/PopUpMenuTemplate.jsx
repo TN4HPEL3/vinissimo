ff62a91277527fa48d2a571c5525c887cbca4ce9
AnnaIstochnikova <anya.istochnikova@gmail.com>
2025-10-18 11:35:23 +0200

    Logos

--- src/components/templates/PopUpMenuTemplate/PopUpMenuTemplate.jsx
+++ src/components/templates/PopUpMenuTemplate/PopUpMenuTemplate.jsx
@@ -1,3 +1,7 @@
+import { NavLink } from 'react-router-dom';
+
+import { IconAtom } from '../../atoms/IconAtom/IconAtom.jsx';
+import { publicAccess } from '../../../helpers/publicAccess.js';
 import { useLanguage } from '../../../helpers/contexts/languageContext.js';
 import { MenuListOrganism } from '../../organisms/MenuListOrganism/MenuListOrganism.jsx';
 
@@ -11,6 +15,17 @@ function PopUpMenuTemplate() {
   return (
     <div className={css.menu}>
       <h2 className={css.title}>{menuTitle}</h2>
+      <div className={css.logos}>
+        <NavLink target="_blank" className={css.logo} to="https://the-farm.pl/">
+          <img
+            className={css.logo_farm}
+            alt="Company logo"
+            src={publicAccess('/LogoTheFarm.png')}
+          />
+        </NavLink>
+
+        <IconAtom className={css.logo} id="logo-desktop" />
+      </div>
       <div className={css.menu__content}>
         <div>
           <MenuListOrganism
