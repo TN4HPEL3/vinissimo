d126846dd647ce92a9da04404dc36e2d356fe8ed
AnnaIstochnikova <anya.istochnikova@gmail.com>
2026-01-24 10:34:15 +0100

    Component

--- src/pages/Valentines/templates/ValentineTemplate.jsx
+++ src/pages/Valentines/templates/ValentineTemplate.jsx
@@ -1,17 +1,24 @@
 import { smoothScrollToTop } from '../../../helpers/smoothScroll.js';
 import { useLanguage } from '../../../helpers/contexts/languageContext.js';
 import { ValentinesCategoryOrganism } from '../organisms/ValentinesCategoryOrganism.jsx';
+import { ParagraphAtom } from '../../../components/atoms/ParagraphAtom/ParagraphAtom.jsx';
 import { ValentinesTitleAtom } from '../atoms/ValentinesTitleAtom/ValentinesTitleAtom.jsx';
 
 function ValentinesTemplate() {
   const { t } = useLanguage();
-  const { valentinesMenu, valentinesMenuCategories, valentinesTitleMenu } = t;
+  const {
+    valentinesMenu,
+    valentinesMenuCategories,
+    valentinesTitleMenu,
+    valentinesDescription,
+  } = t;
   const categories = Object.keys(valentinesMenuCategories);
   smoothScrollToTop();
 
   return (
     <div>
       <ValentinesTitleAtom text={valentinesTitleMenu} />
+      <ParagraphAtom text={valentinesDescription} />
       {categories.map(category => {
         const categoryName = valentinesMenuCategories[category];
         const categoryDetails = valentinesMenu[category];
