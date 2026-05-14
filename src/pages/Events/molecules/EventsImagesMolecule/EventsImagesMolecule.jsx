116e12360691b21282431f8c7bd8b5a7b0bd3613
AnnaIstochnikova <anya.istochnikova@gmail.com>
2026-05-14 16:36:42 +0200

    Fixed mistake

--- src/pages/Events/molecules/EventsImagesMolecule/EventsImagesMolecule.jsx
+++ src/pages/Events/molecules/EventsImagesMolecule/EventsImagesMolecule.jsx
@@ -1,11 +1,11 @@
-import { publicAccess } from '../../../../helpers/publicAccess.js';
+// import { publicAccess } from '../../../../helpers/publicAccess.js';
 
-import css from './EventsImagesMolecule.module.css';
+// import css from './EventsImagesMolecule.module.css';
 
 function EventsImagesMolecule() {
   return (
     <>
-      <img
+      {/* <img
         className={[css['events__content__image']]}
         src={publicAccess('/Frame5.png')}
         alt={'A restaurant tables.'}
@@ -14,7 +14,7 @@ function EventsImagesMolecule() {
         className={[css['events__content__image']]}
         src={publicAccess('/VIP.jpeg')}
         alt={'A restaurant table.'}
-      />
+      /> */}
     </>
   );
 }
